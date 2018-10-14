24403687c9a868e2c2d3d30a97ffc994679cea10
read-0nly <33932119+read-0nly@users.noreply.github.com>
2018-10-14 14:08:32 -0400

    Add files via upload
    FIrst and final version - it does what it needs to do without bugging and any bugs left don't affect what I need to do so ¯\_(ツ)_/¯

---
+++ ImageSorterCs/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace ImageSorterCs
+{
+    partial class ImageSorter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 160F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(438, 276);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(154, 270);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(163, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(272, 270);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(154, 21);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Load Folder";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(163, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(272, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 160F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel2.Controls.Add(this.button1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.textBox1, 1, 0);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(12, 294);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(438, 27);
+            this.tableLayoutPanel2.TabIndex = 3;
+            // 
+            // ImageSorter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(462, 333);
+            this.Controls.Add(this.tableLayoutPanel2);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "ImageSorter";
+            this.Text = "Image Sorter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+    }
+}
+
---
+++ ImageSorterCs/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageSorterCs
+{
+    public partial class ImageSorter : Form
+    {
+        string mnemonics = "123456789qwertyuiop";
+        string root;
+        public int fileIndex = 0;
+        List<string> dirs;
+        public ImageSorter()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void buttonDemo()
+        {
+
+            System.Windows.Forms.Button button;
+            button = new folderButton("c:\\test\\", pictureBox1,"1",this);
+            flowLayoutPanel1.Controls.Add(button);
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            flowLayoutPanel1.Controls.Clear();
+            root = textBox1.Text;
+            dirs = new List<string>(System.IO.Directory.EnumerateDirectories(root));
+            int index = 0;
+            foreach (string dir in dirs)
+            {
+                System.Windows.Forms.Button button;
+                button = new folderButton(dir, pictureBox1, mnemonics[index].ToString(),this);
+                flowLayoutPanel1.Controls.Add(button);
+                index++;
+            }
+            actionButton nextButton = new actionButton("+", actionButton.NEXT,this);
+            actionButton prevButton = new actionButton("0", actionButton.PREVIOUS,this);
+            flowLayoutPanel1.Controls.Add(nextButton);
+            flowLayoutPanel1.Controls.Add(prevButton);
+            fileIndex = 0;
+            loadImage();
+        }
+
+        public void loadImage()
+        {
+            List<string> files = System.IO.Directory.EnumerateFiles(root).ToList<String>();
+            fileIndex = (fileIndex < files.Count ? (fileIndex>0?fileIndex:files.Count-1) : 0);
+            string newImage = files[fileIndex];
+            pictureBox1.ImageLocation = newImage;
+
+        }
+    }
+
+    class folderButton : Button
+    {
+        public PictureBox PicBox;
+        public string Path;
+        public string FolderName;
+        private ImageSorter parentForm;
+        public folderButton(string folderPath, PictureBox picBox, string i, ImageSorter f)
+        {
+            this.Location = new System.Drawing.Point(3, 3);
+            this.Name = "folderButton";
+            this.Size = new System.Drawing.Size(150, 22);
+            this.TabIndex = 1;
+            this.UseVisualStyleBackColor = true;
+            this.Click += new System.EventHandler(this.moveFile);
+            this.Path = folderPath;
+            string[] folderParts = folderPath.Split('\\');
+            FolderName = folderParts[folderParts.Length - 1];
+            this.Text = "&" + i + ") " + FolderName;
+            this.PicBox = picBox;
+            this.parentForm = f;
+        }
+        public void moveFile(object sender, EventArgs e)
+        {
+            System.IO.Directory.Move(PicBox.ImageLocation,Path+"\\"+(System.IO.Path.GetFileName(PicBox.ImageLocation)));
+            parentForm.loadImage();
+        }
+    }
+
+    class actionButton : Button
+    {
+        public int Mode = 0;
+        public static int NEXT = 1;
+        public static int PREVIOUS = 2;
+        private ImageSorter formRef;
+
+        public actionButton(string mnemonic, int mode, ImageSorter f)
+        {
+            this.Location = new System.Drawing.Point(3, 3);
+            this.Name = "folderButton";
+            this.Size = new System.Drawing.Size(150, 22);
+            this.TabIndex = 1;
+            this.UseVisualStyleBackColor = true;
+            this.Click += new System.EventHandler(this.doAction);
+            this.Text = "&"+mnemonic+")"+(mode==1?">":(mode==2?"<":"wut"));
+            Mode = mode;
+            formRef = f;
+        }
+        public void doAction(object sender, EventArgs e)
+        {
+            if (Mode == actionButton.NEXT)
+            {
+                formRef.fileIndex++;
+                formRef.loadImage();
+            }
+            else if (Mode == actionButton.PREVIOUS)
+            {
+                formRef.fileIndex--;
+                formRef.loadImage();
+            }
+        }
+    }
+
+}
